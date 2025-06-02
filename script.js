c3028b366a23547f7bfcd05867a6a9f9e3601245
user9626 <349987800@gapps.yrdsb.ca>
2025-06-02 08:23:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
-fetch("data/timetable.json")
-.then((res) => {
-  if (!res.ok) throw new Error(response.status)
-  return res.text()
-})
-.then((text) => {
-  JSON.parse(text)
-})
-.catch((e) => {
-  console.error("Couldn't Fetch Timetable Data: "+e)
-})
+let dayOffset = 0
+let colorsData
+let timetableData
+
+async function getJSON(url) {
+  return fetch(url)
+    .then((response)=>response.json())
+    .then((text)=>{return (text)})
+    .catch((e) => {
+      console.error(`Failed to load ${url}: ${e}`)
+      window.setTimeout(()=>{window.location.reload()}, 5000)
+    })
+}
+
+async function loadJSON() {
+  colorsData = await getJSON("data/colors.json")
+  timetableData = await getJSON("data/timetable.json")
+  displayTimetable()
+  window.setInterval(displayTimetable, 10000)
+}
+
+function displayTimetable() {
+  // Setup Date
+  const date = new Date("May 23, 2025 09:01:00")
+  console.log(date.getHours().toString()+":"+date.getMinutes().toString())
+  const day = date.getDay() + dayOffset
+  if (day == 0 || day == 6) {
+    document.getElementById("subjects").innerHTML = "<p style='color: white;text-align: center;font-size: 1.5em;'>No school today.</p>"
+    return
+  }
+
+  // Add Subjects To Page
+  let timetable = timetableData[day]
+  let timetableKeys = Object.keys(timetable)
+  let timetableValues = Object.values(timetable)
+  let timetableHTML = ""
+  for (let i = 0; i < timetableKeys.length; i++) {
+    const backgroundColor = colorsData[timetableValues[i].toLowerCase()]
+    const subjecttime = parseFloat(timetableKeys[i])
+    var nowtime;
+    if (date.getMinutes() < 10) {
+      nowtime = parseFloat((date.getHours().toString() + ".0" + date.getMinutes().toString()))
+    } else {
+      nowtime = parseFloat((date.getHours().toString() + "." + date.getMinutes().toString()))
+    }
+    if (subjecttime < nowtime) {
+      if (nowtime < parseFloat(timetableKeys[i+1])) {
+        timetableHTML += `<p class="subjectnow" style="background-color:${backgroundColor};">${timetableValues[i]}</p><p class="subjecttime">${timetableKeys[i]}</p></br>`
+      } else {
+        timetableHTML += `<p class="subjectend"">${timetableValues[i]}</p><p class="subjecttime">${timetableKeys[i]}</p></br>`
+      }
+    } else {
+        timetableHTML += `<p class="subjectstart" style="background-color: ${backgroundColor};">${timetableValues[i]}</p><p class="subjecttime">${timetableKeys[i]}</p></br>`
+    }
+  }
+
+  // Display Subjects
+  document.getElementById("subjects").innerHTML = timetableHTML
+}
+
+// Initilize Timetable From JSON
+loadJSON()
